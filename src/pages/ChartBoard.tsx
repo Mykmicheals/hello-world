59405572adb88b879be651100a865bd0149189d5
Andrew Micheal <mykmicheals@gmail.com>
2023-05-24 13:32:17 +0100

    working user and admin panel

--- src/pages/ChartBoard.tsx
+++ src/pages/ChartBoard.tsx
@@ -12,6 +12,12 @@ Chart.register(CategoryScale);
 
 function ChartBoard() {
   const token = useStoreState((state: storeTypes) => state.token);
+  const isAdmin = useStoreState((state: storeTypes) => state.isAdmin);
+
+  const [user, setUser] = useState<any>([]);
+  const [selectedUser, setSelectedUser] = useState(null);
+
+  console.log(token);
 
   const [data, setData] = useState({
     labels: [],
@@ -57,15 +63,122 @@ function ChartBoard() {
       }
     };
 
-    fetchData();
+    // const fetchAdmin = async () => {
+    //   try {
+    //     const response = await fetch(`${appUrl}/app/admin/`, {
+    //       headers: {
+    //         Authorization: `Bearer ${token}`,
+    //       },
+    //     });
+
+    //     const adminData = await response.json();
+    //     console.log(adminData);
+
+    //     if (adminData.length > 0) {
+    //       const firstUser = adminData[0];
+    //       setUser({
+    //         name: firstUser.trader_name,
+    //         total_balance: firstUser.data[0]?.trader.total_balance,
+    //       });
+
+    //       const values = firstUser?.data.map((item: any) => item.profit_loss);
+    //       const labels = firstUser?.data.map((item: any) =>
+    //         moment(item.timestamp).format("HH:mm")
+    //       );
+
+    //       setData({
+    //         ...data,
+    //         labels,
+    //         datasets: [
+    //           {
+    //             ...data.datasets[0],
+    //             data: values,
+    //           },
+    //         ],
+    //       });
+    //     }
+    //   } catch (error) {
+    //     console.error("Error fetching performance data:", error);
+    //   }
+    // };
+
+    const fetchAdmin = async () => {
+      try {
+        const response = await fetch(`${appUrl}/app/admin/`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        const adminData = await response.json();
+        console.log(adminData);
+
+        if (adminData.length > 0) {
+          const firstUser = adminData[0];
+          setSelectedUser(firstUser);
+          setUser({
+            name: firstUser.trader_name,
+            total_balance: firstUser.data[0]?.trader.total_balance,
+          });
+
+          const values = firstUser.data.map((item: any) => item.profit_loss);
+          const labels = firstUser.data.map((item: any) =>
+            moment(item.timestamp).format("HH:mm")
+          );
+
+          setData({
+            ...data,
+            labels,
+            datasets: [
+              {
+                ...data.datasets[0],
+                data: values,
+              },
+            ],
+          });
+        }
+      } catch (error) {
+        console.error("Error fetching performance data:", error);
+      }
+    };
+
+    const fetchUserDetails = async () => {
+      try {
+        const response = await fetch(`${appUrl}/app/data/`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        const data = await response.json();
+        setUser(data);
+        console.log(data);
+      } catch (error) {
+        console.error("Error fetching performance data:", error);
+      }
+    };
+
+    !isAdmin ? fetchData() : fetchAdmin();
+    !isAdmin && fetchUserDetails();
   }, []);
 
-  return (
-    <div className="w-2/3 mt-28">
-
-      <LineChart chartData={data} />
-    </div>
-  );
+  if (isAdmin === false) {
+    return (
+      <div className="w-2/3 mt-28">
+        <div className="flex gap-32">
+          <div>User:{user[0]?.name}</div>
+          <div>Ballance: ${user[0]?.total_balance}</div>
+        </div>
+        <LineChart chartData={data} />
+      </div>
+    );
+  } else {
+    return (
+      <div className="w-2/3 mt-28">
+      
+        <LineChart chartData={data} />
+      </div>
+    );
+  }
 }
 
 export default ChartBoard;
